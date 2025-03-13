daee49359447da737a45592b798afcd7182a6d70
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2025-03-13 11:06:32 +0100

    tränat på JS, variabler, konkatenering

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,18 @@
+
+// var ålder = 18;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+
+var fornamn = "Dante";
+var efternamn = "Harhoff";
+var fullNamn = fornamn + " " + efternamn;
+var hobby = "AHHHHHHHHHHH";
+var kostnad = 10000000;
+
+
+var mening = fullNamn + "'s hobby är " + hobby + ".\nMin hobby kostar " + kostnad + " kr/år";
+alert(mening);
