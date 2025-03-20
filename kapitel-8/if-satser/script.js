1ea22cbcf829d193b6f8782f4795cf6d370567c5
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2025-03-20 11:08:10 +0100

    tränat på if-satser

---
+++ kapitel-8/if-satser/script.js
@@ -0,0 +1,32 @@
+// var tal1 = parseInt(prompt("Hur gammal är du?"))
+
+// if (tal1 >= 15) {
+//     document.write("Du får se filmen")
+// } else {
+//     document.write("Du får inte se filmen")
+// }
+
+
+// var tal2 = parseInt(prompt("hur många grader är det ute"))
+
+// if (tal2 > 20) {
+//     alert("Det är varmt ute!")
+// } else {
+//     alert("Det är kallt ute!")
+// }
+
+
+var favoritDjur = prompt("Vad är ditt favorit djur?")
+
+if (favoritDjur == "katt") {
+    console.log("Katter är också mina favorit djur!!")
+    alert("Katter är också mina favorit djur!!")
+}
+else if (favoritDjur == "hund") {
+    console.log("Jag gillar också hundar.")
+    alert("Jag gillar också hundar.")
+}
+else {
+    console.log("BOOOOOOO")
+    alert("BOOOOOOO")
+}
