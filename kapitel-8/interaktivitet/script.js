6cf56c4b95039eec75ea09d27cf62962dd1bb99f
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2025-03-27 11:08:33 +0100

    tränat på onclick, funktioner, querySelector

---
+++ kapitel-8/interaktivitet/script.js
@@ -0,0 +1,15 @@
+function DarkPage() {
+    document.body.style = "background: #000; color: #FFF;";
+    document.querySelector("h1").style = "font-size: 100px;"
+}
+function WhitePage() {
+    document.body.style = "background: #fff; color: #000;";
+    document.querySelector("h1").style = "font-size: 50px;"
+}
+function konstigRubrik() {
+    document.querySelector("h1").style = "font-size: 100px; text-transform: lowercase; text-shadow: 5px 5px 5px yellowgreen; background: khaki;"
+}
+function VisaText() {
+    document.querySelector("p").style = "visibility: visible;"
+}
+
